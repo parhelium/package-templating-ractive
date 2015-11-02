ecd1d55384968f4f9406115ed9db07a8a7f657ea
parhelium <kubawyrobek@fastmail.com>
2015-11-02 13:04:50 +0100

    parhelium:ractive is implied

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name:'parhelium:templating-ractive',
   summary: "Allows ractive templates to be defined in .ract files",
-  version: "0.7.3",
+  version: "0.7.3_1",
   git:'https://github.com/parhelium/package-templating-ractive'
 });
 
@@ -17,6 +17,7 @@ Package._transitional_registerBuildPlugin({
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.2.2');
   api.use('parhelium:ractive@0.7.3');
+  api.imply('parhelium:ractive@0.7.3');
 
   api.addFiles('templating-ractive.js');
   api.export('Ract', ['client', 'server']);
